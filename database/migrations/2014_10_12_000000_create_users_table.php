8644711ba4c9978bd75ac2e60ea42c6fde47ed6a
Zeeshan123123 <37213131+Zeeshan123123@users.noreply.github.com>
2020-06-20 09:49:35 -0700

    db Photo issue resolved
    Made nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->string('type')->default('user');
             $table->mediumText('bio')->nullable();
-            $table->string('photo')->default('profile.png');
+            $table->string('photo')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
